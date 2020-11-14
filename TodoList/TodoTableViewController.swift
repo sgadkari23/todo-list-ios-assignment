ecf13015eff4c1b8d00b62137838b29fe4941d53
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-11-14 16:56:38 -0600

    Final code changes commit

--- TodoList/TodoTableViewController.swift
+++ TodoList/TodoTableViewController.swift
@@ -22,13 +22,13 @@ class TodoTableViewCell: UITableViewCell {
             
             attributeString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
             todoTaskName.attributedText = attributeString
-            todoTaskStatus?.text = "Completed"
+            todoTaskStatus?.text = "Overdue"
            
         }else{
             
             attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
             todoTaskName.attributedText = attributeString
-            todoTaskStatus?.text = "Overdue"
+            todoTaskStatus?.text = "Completed"
         }
     
     }
@@ -39,14 +39,15 @@ class TodoTableViewController:  UIViewController, UITableViewDataSource, UITable
     @IBOutlet var todoTable: UITableView!
     
     var taskNameArray = ["Shopping List","Grocery List","Travel checklist"]
+    var taskStatus = ["Overdue","Overdue","Friday November 14,2020"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         todoTable.delegate = self
         todoTable.dataSource = self
         self.todoTable.rowHeight = 60.0
+        self.title = "Todo"
     }
-    
    
     
     // MARK: - Table view data source
@@ -62,80 +63,9 @@ class TodoTableViewController:  UIViewController, UITableViewDataSource, UITable
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoTableCell", for: indexPath) as! TodoTableViewCell
         cell.todoTaskName?.text = taskNameArray[indexPath.row]
-        cell.todoTaskStatus?.text = "Completed"
-        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: (cell.todoTaskName?.text)!)
-       
-        if (cell.todoTaskSwitchButton.isOn) {
-        
-            attributeString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
-            cell.todoTaskName.attributedText = attributeString
-            cell.todoTaskStatus?.text = "Completed"
-        }else{
-            
-            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
-            cell.todoTaskName.attributedText = attributeString
-            cell.todoTaskStatus?.text = "Overdue"
-           
-        }
-        //cell.accessoryView = UISwitch()
-        //cell.todoTaskEdit.image = UIImage(named: "imgCat")
+        cell.todoTaskStatus?.text = taskStatus[indexPath.row]
         return cell
 
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
